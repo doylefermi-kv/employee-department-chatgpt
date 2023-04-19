f4985dfccab250c2cdb5121b2a7ff4093619843e
Ajai Sunny Joseph <ajai@keyvalue.systems>
2023-04-19 12:41:45 +0530

    remove 'saltRounds' var

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -2,8 +2,6 @@ import { Request, Response } from 'express';
 import { EmployeeService } from '../services/employee.service';
 import { CreateEmployeeDto } from '../dto/create-employee.dto';
 
-const saltRounds = 10;
-
 export class LoginController {
   private employeeService: EmployeeService;
 
