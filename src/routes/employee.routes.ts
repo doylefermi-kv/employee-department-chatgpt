5161fc7cff39830d8a7740869fe3e29cb8769524
Doyle Fermi <doyle@keyvalue.systems>
2023-04-19 12:06:35 +0530

    add: dto validation in employee
    fix: this is undefined in controller context
    > The arrow functions inherit the this value from their surrounding context, which is the DepartmentRoutes instance in this case.

--- src/routes/employee.routes.ts
+++ src/routes/employee.routes.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import { EmployeeRepository } from '../repositories/employee.repository';
 import { EmployeeService } from '../services/employee.service';
 import { EmployeeController } from '../controllers/employee.controller';
+import { validateDto } from '../middleware/validate.dto';
+import { CreateEmployeeDto } from '../dto/create-employee.dto';
+import { EditEmployeeDto } from '../dto/edit-employee.dto';
 
 export class EmployeeRoutes {
   private router = Router();
@@ -15,11 +18,11 @@ export class EmployeeRoutes {
   }
 
   public async routes() {
-    this.router.get('/', this.employeeController.getAllEmployees);
-    this.router.post('/', this.employeeController.createEmployee);
-    this.router.get('/:id', this.employeeController.getEmployeeById);
-    this.router.put('/:id', this.employeeController.updateEmployee);
-    this.router.delete('/:id', this.employeeController.deleteEmployee);
+    this.router.get('/', (req, res) => this.employeeController.getAllEmployees(req, res));
+    this.router.post('/', validateDto(CreateEmployeeDto), (req, res) => this.employeeController.createEmployee(req, res));
+    this.router.get('/:id', (req, res) => this.employeeController.getEmployeeById(req, res));
+    this.router.put('/:id', validateDto(EditEmployeeDto), (req, res) => this.employeeController.updateEmployee(req, res));
+    this.router.delete('/:id', (req, res) => this.employeeController.deleteEmployee(req, res));
   }
 
   public getRouter() {
